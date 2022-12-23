fec99ba0479ff4a3c2cdbe07f11d14d0f5b50658
mvpc-11232 <mvp2612@hotmail.com>
2022-12-23 07:56:38 +0530

    Auto Push Commit

--- tf-mutable/main.tf
+++ tf-mutable/main.tf
@@ -1,9 +1,6 @@
 module "cart" {
-  source                    = "./vendor/modules/docdb"  
-  ENV                       = var.ENV 
-  DOCDB_PORT                = var.DOCDB_PORT 
-  DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
-  DOCDB_PORT_INSTANCE_CLASS = var.DOCDB_PORT_INSTANCE_CLASS
+  source                    = "./vendor/modules/app"  
+  COMPONENT                 = var.COMPONENT 
 
 }
 natives to make it flexible ?
