80f8f6afbbfcc61dc5e6bc301e90edb14e1efe02
mvpc-11232 <mvp2612@hotmail.com>
2022-12-23 09:07:10 +0530

    Auto Push Commit

--- tf-mutable/main.tf
+++ tf-mutable/main.tf
@@ -1,10 +1,8 @@
 module "cart" {
   source                    = "./vendor/modules/app"  
   COMPONENT                 = var.COMPONENT 
-
+  INSTANCE_TYPE             = var.INSTANCE_TYPE 
+  SPOT_INSTANCE_COUNT       = var.SPOT_INSTANCE_COUNT
+  OD_INSTANCE_COUNT         = var.OD_INSTANCE_COUNT 
+  APP_PORT                  = var.APP_PORT
 }
-
-variable "INSTANCE_TYPE" {}
-variable "SPOT_INSTANCE_COUNT" {}
-variable "OD_INSTANCE_COUNT" {}
-variable "APP_PORT"  {}
